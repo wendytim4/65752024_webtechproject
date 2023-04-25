a01e0512a7f41b73e6294122951ff17a2347fcdb
wendytim4 <109157790+wendytim4@users.noreply.github.com>
2023-04-25 23:23:24 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login Page</title>
+</head>
+<body>
+	 
+	 <h2>Login Here</h2>
+	 <!-- login form -->
+	 <form>
+	  <label>User Name:</label><br>
+	  <input type="text"name="uname"><br>
+	  <label>User Pass:</label><br>
+	  <input type="password" name="upass"><br><br>
+	  <input type="submit" name="login" value="Login">
+	</form> 
+
+	<!-- link to register -->
+	<a href="register.php">Go to Register Page</a>
+
+</body>
+</html>
